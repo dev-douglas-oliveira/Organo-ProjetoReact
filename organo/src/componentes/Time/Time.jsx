fab3c0bb8cc56c494cab0c7cfa2dc467dce8098a
Douglas Oliveira <dougmoroli@gmail.com>
2023-04-23 14:28:18 -0300

    elevando a lista de times

--- organo/src/componentes/Time/Time.jsx
+++ organo/src/componentes/Time/Time.jsx
@@ -1,9 +1,16 @@
 import "./Time.css";
 
 export const Time = (props) => {
+    const cssTime = { backgroundColor: props.corSecundaria };
+    const cssBordaTime = { borderColor: props.corPrimaria };
+    //pode escrever assim
+    //style={cssTime}
+
+    // ou assim: style={{ backgroundColor: props.corSecundaria }}
+
     return (
-        <section className="time">
-            <h3>{props.nome}</h3>
+        <section className="time" style={cssTime}>
+            <h3 style={cssBordaTime}>{props.nome}</h3>
         </section>
     );
 };
