9e2babd048bf24858231793a89589ae3886130e4
Douglas Oliveira <dougmoroli@gmail.com>
2023-05-01 13:35:10 -0300

    adicionando input type color nos times

--- organo/src/componentes/Time/Time.jsx
+++ organo/src/componentes/Time/Time.jsx
@@ -10,10 +10,16 @@ export const Time = (props) => {
     // ou assim: style={{ backgroundColor: props.corSecundaria }}
 
     return (
-        //props.colaborares.length > 0 && serve para verificar se a array de colaboradores é maio que zero, se for exibe a section
+        //props.colaborares.length > 0 && serve para verificar se a array de colaboradores é maior que zero, se for exibe a section
         // tbm pode escrever da seguinte forma: props.colaborares.length > 0 ? : ""
         props.colaboradores.length > 0 ? (
             <section className="time" style={cssTime}>
+                <label className="label-cor">Trocar cor do Time</label>
+                <input
+                    value={props.corPrimaria}
+                    type="color"
+                    className="input-cor"
+                />
                 <h3 style={cssBordaTime}>{props.nome}</h3>
                 <div className="colaboradores">
                     {props.colaboradores.map((colaborador, indice) => {
